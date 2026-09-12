f04358c35a7d9193def6313468d2774c711578ae
ALESSANDRO RAPITI <alessandro.rapiti44@gmail.com>
2026-09-12 12:14:23 +0200

    feat: wire pending prices workflow

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Layout, type PageKey } from "@/components/Layout";
 import { useAuctionData } from "@/hooks/useAuctionData";
 import { DashboardPage } from "@/pages/DashboardPage";
 import { AuctionPage } from "@/pages/AuctionPage";
+import { PendingPricesPage } from "@/pages/PendingPricesPage";
 import { MarketPage } from "@/pages/MarketPage";
 import { FinalReportPage } from "@/pages/FinalReportPage";
 import { PlayersPage } from "@/pages/PlayersPage";
@@ -18,7 +19,8 @@ export function App() {
   const data = api.data;
   return <Layout page={page} onPage={setPage}>
     {page === "dashboard" && <DashboardPage data={data} onUndoPurchase={api.undoPurchase} />}
-    {page === "auction" && <AuctionPage data={data} onAuction={api.updateAuction} onPurchase={api.addPurchase} onPurchaseBundle={api.addPurchaseBundle} onBid={api.addBid} />}
+    {page === "auction" && <AuctionPage data={data} onAuction={api.updateAuction} onPurchase={api.addPurchase} onPurchaseBundle={api.addPurchaseBundle} onBid={api.addBid} onOpenPrices={() => setPage("prices")} />}
+    {page === "prices" && <PendingPricesPage data={data} onSave={api.finalizePendingPrices} />}
     {page === "market" && <MarketPage data={data} onUndoPurchase={api.undoPurchase} onDeleteBid={api.removeBid} />}
     {page === "report" && <FinalReportPage data={data} onAuction={api.updateAuction} />}
     {page === "players" && <PlayersPage data={data} onPlayer={api.updatePlayer} onPlayers={api.updatePlayers} onCatalog={api.replaceCatalog} />}
